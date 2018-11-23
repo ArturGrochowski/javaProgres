316f105cadc69562de9e3b44a32a47b086e7d1cd
ArturGrochowski <44144541+ArturGrochowski@users.noreply.github.com>
2018-11-23 12:55:46 +0100

    Loop and Array

---
+++ src/TestMain.java
@@ -0,0 +1,13 @@
+public class TestMain {
+    public static void main(String[] args) {
+        WaterBottle[] butelk = new WaterBottle[10];
+        for(int i = 0; i<butelk.length; i++){
+            double liters = Math.round((Math.random() * 10) * 10.0)/10.0;
+            butelk[i] = new WaterBottle(liters);
+        }
+        for(WaterBottle b : butelk){
+            System.out.println(b.getIleLitrow());
+        }
+
+    }
+}
---
+++ src/WaterBottle.java
@@ -0,0 +1,11 @@
+public class WaterBottle {
+    private double ileLitrow;
+
+    public WaterBottle(double liters) {
+        ileLitrow = liters;
+    }
+    double getIleLitrow(){
+        return ileLitrow;
+    }
+}
+
