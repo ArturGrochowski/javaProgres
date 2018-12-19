b882d346127164b9b1f1c1a9023cf12e306a52fd
ArturGrochowski <44144541+ArturGrochowski@users.noreply.github.com>
2018-12-19 16:23:42 +0100

    Bank Account

--- NestedClass/src/MainNested.java
+++ NestedClass/src/MainNested.java
@@ -1,4 +1,5 @@
 public class MainNested {
+
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount(1000);
         System.out.println("Your balance befora: " + bankAccount.getBalance());
@@ -8,5 +9,6 @@ public class MainNested {
         System.out.println("Hello Mr. Artur. Yesterday your balance was: " + finalAccount.getBalance());
         finalAccount.createAccount(12, 100);
         System.out.println("Your current balance is: " + finalAccount.getBalance());
+
     }
 }
