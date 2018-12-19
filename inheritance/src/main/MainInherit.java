1147abe2e45f5c3d08e5616fdbb3406ff9e8d209
ArturGrochowski <44144541+ArturGrochowski@users.noreply.github.com>
2018-12-19 16:26:11 +0100

    Monsters

--- inheritance/src/main/MainInherit.java
+++ inheritance/src/main/MainInherit.java
@@ -8,13 +8,9 @@ import monsters.Zombie;
 public class MainInherit {
     public static void main(String[] args) {
         IActions[] nameTab = new IActions[4];
-//        System.out.println("Making Monster with 2 arguments:");
         Monster m = new Monster(10, 100);
-//        System.out.println("Making Zombie with no arguments:");
         Zombie z = new Zombie();
-//        System.out.println("Making Skeleton with 2 arguments:");
         Skeleton s = new Skeleton(15, 20, "Sword and Bow");
-//        System.out.println("Polymorphism example with no arguments:");
         Monster mS = new Skeleton(); /* Polymorphism | in that case if I implement someMethod() inside of Skeleton Class I won't be able to access this method
          by mS.someMethod(); because I will refer to Monster class where someMethod() doesn't exist. I can create an object Skeleton mS = new Skeleton
          and then I will be able do mS.someMethod(); */
