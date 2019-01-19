b020a37d39ef12b121f53833dd0a56692238c812
ArturGrochowski <44144541+ArturGrochowski@users.noreply.github.com>
2019-01-19 17:16:44 +0100

    Clock JComboBox

--- clock/src/Clock.java
+++ clock/src/Clock.java
@@ -7,7 +7,7 @@ import java.util.GregorianCalendar;
 class Clock extends JFrame {
     Clock(){
         super("Clock");
-        this.setBounds(300,300,300,200); // if I have pack() inside initComponents() I don't need to set Size of the frame, just location.
+        this.setBounds(300,300,350,200); // if I have pack() inside initComponents() I don't need to set Size of the frame, just location.
 //        this.setLocation(300,300);
         initComponents();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -15,10 +15,7 @@ class Clock extends JFrame {
 
     private void initComponents() {
 //        setLayout(new BorderLayout());
-        panel.add(lable);
-        multiPanel.add(panelPageEnd);
-//        panelPageEnd.add(multiPanel);
-//        panelPageEnd.add(time);
+
 
         ActionListener watch = new ClockListener();
 
@@ -30,8 +27,6 @@ class Clock extends JFrame {
         new MakeButton().makeButtonColor("Color", lable, panelCenter);
         JCheckBox bold = new JCheckBox("Bold");
         JCheckBox italic = new JCheckBox("Italic");
-        panelNorth.add(bold);
-        panelNorth.add(italic);
         lable.setFont(new Font("Arial", Font.PLAIN, lable.getFont().getSize()));
         class ChackBoxHandler implements ActionListener{
 
@@ -54,13 +49,51 @@ class Clock extends JFrame {
         this.getContentPane().add(panelCenter, BorderLayout.CENTER);
 //        multiPanel.setLayout(new BorderLayout());
 //        this.getContentPane().add(panel, BorderLayout.WEST);
+
+        Timer timer = new Timer(1000, watch);
+        timer.start();
+        comboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                String fontName = (String) comboBox.getSelectedItem();
+                System.out.println(comboBox.getSelectedItem());
+                     lable.setFont(new Font(comboBox.getSelectedItem()+"", lable.getFont().getStyle(), lable.getFont().getSize()));
+            }
+        });
+        comboBox.addItem(new FontHandler("Arial", lable.getFont().getStyle()));
+        comboBox.addItem(new FontHandler("Aharoni", lable.getFont().getStyle()));
+        comboBox.addItem(new FontHandler("Corbel", lable.getFont().getStyle()));
+        comboBox.addItem(new FontHandler("David", lable.getFont().getStyle()));
+        comboBox.addItem(new FontHandler("Dotum", lable.getFont().getStyle()));
+        comboBox.addItem(new FontHandler("Impact", lable.getFont().getStyle()));
+        comboBox.addItem(new FontHandler("Monospaced", lable.getFont().getStyle()));
+
+        //        panel.add(lable);
+        multiPanel.add(panelPageEnd);
+        multiPanel.add(comboBox);
+        panelNorth.add(bold);
+        panelNorth.add(italic);
         multiPanel.add(panel, BorderLayout.NORTH);
         multiPanel.add(timeLable, BorderLayout.SOUTH);
         multiPanel.add(time, BorderLayout.SOUTH);
-        Timer timer = new Timer(1000, watch);
-        timer.start();
+        panelCenter.add(lable);
+//        panelPageEnd.add(multiPanel);
+//        panelPageEnd.add(time);
+
 
 //        pack();
+    }
+    private class FontHandler{
+        public FontHandler(String fontName, int style){
+            this.fontName = fontName;
+            this.style = style;
+        }
+        @Override
+        public String toString(){
+            return this.fontName;
+        }
+        private String fontName;
+        private int style;
     }
 
 
@@ -68,12 +101,14 @@ class Clock extends JFrame {
     JPanel panelNorth = new JPanel();
     JPanel panelCenter = new JPanel();
     JPanel panel = new JPanel();
-    JLabel lable = new JLabel("TIMEX");
+    JLabel lable = new JLabel(" TIMEX ");
     JLabel timeLable = new JLabel("Time: ");
     JLabel time = new JLabel(getTime());
     JPanel multiPanel = new JPanel();
     JLabel lableBox = new JLabel();
     ButtonGroup buttonGrSize = new ButtonGroup(); // this group make one choice from all added buttons possible.
+    JComboBox<FontHandler> comboBox = new JComboBox<FontHandler>();
+
 
     private class ClockListener implements ActionListener {
         @Override
