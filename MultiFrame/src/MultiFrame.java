ce682300bc3d4a6aa863d0cb192c310cc0b192c7
ArturGrochowski <44144541+ArturGrochowski@users.noreply.github.com>
2019-01-24 12:18:49 +0100

    MultiFrame Border

--- MultiFrame/src/MultiFrame.java
+++ MultiFrame/src/MultiFrame.java
@@ -1,6 +1,12 @@
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import static javax.swing.BorderFactory.createTitledBorder;
 
 public class MultiFrame extends JFrame {
     static int xy = 300;
@@ -8,12 +14,17 @@ public class MultiFrame extends JFrame {
         initComponent();
     }
 
-    private void initComponent() {
+    public void initComponent() {
         this.setTitle("MultiFrame");
         this.setBounds(xy,xy,300,200);
+        this.getContentPane().setLayout(new GridLayout(2,1));
         this.getContentPane().add(panel);
+        this.getContentPane().add(panelEdge);
         this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         panel.add(newFrameButton);
+        panel.setBorder(createTitledBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED), "Creator", TitledBorder.CENTER, TitledBorder.TOP));
+        addRadioButton("EtchedBorder", BorderFactory.createEtchedBorder());
+        addRadioButton("LoweredBevelBrd", BorderFactory.createLoweredBevelBorder());
         newFrameButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -23,6 +34,15 @@ public class MultiFrame extends JFrame {
         });
     }
 
+    public void addRadioButton (String name, Border border){
+        JRadioButton radioButton = new JRadioButton(name);
+        radioButton.addItemListener(e -> panelEdge.setBorder(createTitledBorder(border, name)));
+        buttonGroup.add(radioButton);
+        panelEdge.add(radioButton);
+    }
+
     private JButton newFrameButton = new JButton("New Frame");
     private JPanel panel = new JPanel();
+    private JPanel panelEdge = new JPanel();
+    private ButtonGroup buttonGroup = new ButtonGroup();
 }
